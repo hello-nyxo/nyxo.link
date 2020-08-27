83a2085f9849311fcbd855bb49c99f94c97bebc3
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-08-27 19:08:46 +0300

    Update ream and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./src/data/`,
+        name: `links`,
+        path: `${__dirname}/src/data`,
       },
     },
     `gatsby-transformer-sharp`,
